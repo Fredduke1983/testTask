379d069ca1dc5066f4880764d7816634f9278edd
Nechai_Ihor <fredduke1983@gmail.com>
2023-05-17 10:30:00 +0300

    FIXED: deploy github

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/testtask/',
+  base: '/testTask',
   plugins: [react()],
   server: {
     port: 3030,
